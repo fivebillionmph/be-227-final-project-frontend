c8cbf28f4d6665e8745f0144c3a25637ab739d6e
James Go <jamescookgo@gmail.com>
2018-11-24 01:59:32 -0800

    authorization update

--- securep2p227/keys.py
+++ securep2p227/keys.py
@@ -11,14 +11,14 @@ _PUBLIC_KEY_FILE_NAME = "public-key"
 _INFO_FILE_NAME = "info"
 
 def getKey(key_dir, name):
-	if not os.isdir(os.path.join(key_dir, name)):
+	if not os.path.isdir(os.path.join(key_dir, name)):
 		raise Exception("key directory does not exist")
 
 	private_key_file = os.path.join(key_dir, name, _PRIVATE_KEY_FILE_NAME)
 	public_key_file = os.path.join(key_dir, name, _PUBLIC_KEY_FILE_NAME)
 	info_file = os.path.join(key_dir, name, _INFO_FILE_NAME)
 
-	if not os.isfile(private_key_file) or not os.isfile(public_key_file) or not os.isfile(info_file):
+	if not os.path.isfile(private_key_file) or not os.path.isfile(public_key_file) or not os.path.isfile(info_file):
 		raise Exception("private or public key missing")
 
 	with open(private_key_file, mode="rb") as f:
@@ -61,8 +61,14 @@ def genKey(key_dir, name, user_name, user_organization):
 
 	return Key(private_key, public_key, dir_path, user_name, user_organization)
 
-def publicKeyToString(public_key):
+def publicKeyToPemString(public_key):
 	return public_key.save_pkcs1().decode("utf-8")
+
+def pemStringToPublicKey(pem_string):
+	return rsa.PublicKey.load_pkcs1(pem_string.encode("utf-8"))
+
+def signatureMessageToString(message):
+	return message["public_key"] + str(message["start_time"]) + str(message["end_time"]) + message["check_server"] + message["message_key"]
 
 class Host:
 	protocol = "http"
@@ -100,7 +106,7 @@ class Key:
 		self._organization = organization
 
 	def publicKeyString(self):
-		return publicKeyToString(self._public_key)
+		return publicKeyToPemString(self._public_key)
 
 	def register(self, host):
 		url, method = host.registerURL()
@@ -160,16 +166,23 @@ class Key:
 		host = host._fqdn
 		message_key = str(random.randint(0, 1000000000))
 		message = {
-			"public_key": publicKeyToString(public_key),
+			"public_key": publicKeyToPemString(public_key),
 			"start_time": start_unix,
 			"end_time": end_unix,
 			"check_server": host,
 			"message_key": message_key,
 		}
-		message_str = message["public_key"] + str(message["start_time"]) + str(message["end_time"]) + message["check_server"] + message["message_key"]
+		message_str = signatureMessageToString(message)
 		signature = rsa.sign(message_str.encode("utf-8"), self._private_key, "SHA-256")
 		signature_base64 = base64.b64encode(signature).decode("utf-8")
 		return (message, signature_base64)
+
+	def encrypt(self, public_key_string, message):
+		public_key = pemStringToPublicKey(public_key_string)
+		return rsa.encrypt(message.encode("utf-8"), public_key)
+
+	def decrypt(self, crypt_message):
+		return rsa.decrypt(message, self._private)
 
 class Session:
 	def __init__(self, host, key):
@@ -204,8 +217,79 @@ class Session:
 			"signature": signature,
 			"message": message,
 			"session_id": self._session_id,
-			"signee_public_key": publicKeyToString(public_key),
+			"signee_public_key": publicKeyToPemString(public_key),
 		}
 		response = requests.request(method, url, json=req_data)
 		if not response.ok:
 			raise Exception(response.text)
+
+class Permission:
+	def __init__(self, permission_dir, name):
+		self._file_path = os.path.join(permission_dir, name)
+		self._config = None
+
+		if not os.path.isdir(permission_dir):
+			raise Exception("permission directory does not exist")
+
+		if os.path.isfile(self._file_path):
+			self._config = json.loads(open(path).read())
+		else:
+			self._config = {
+				"authorized_keys": [],
+			}
+
+	def updateConfigFile(self):
+		config_data = json.dumps(self._config)
+		open(self._file_path, "w").write(config_data)
+
+	def addAuthorizedKey(self, key_pem_string, name, organization):
+		for ak in self._config["authorized_keys"]:
+			if ak["key"] == key_pem_string:
+				return # key already exists
+		try:
+			public_key_obj = pemStringToPublicKey(key_pem_string)
+		except:
+			return
+		self._config["authorized_keys"].append({
+			"key": key_pem_string,
+			"name": name,
+			"organization": organization,
+		})
+		self.updateConfigFile()
+
+	def authorize(self, public_key_string, signature_base64, message, signer_public_key_string):
+		for ak in self._config["authorized_keys"]:
+			if ak["key"] == public_key_string:
+				return True
+
+		if not signature_base64 or not message or not signer_public_key_string:
+			return False
+
+		found = False
+		for ak in self._config["authorized_keys"]:
+			if ak["key"] == signer_public_key_string:
+				found = True
+				break
+		if not found:
+			return False
+
+		now = int(time.time())
+		if message["public_key"] != public_key_string:
+			return False
+		if message["start_time"] > now or message["end_time"] < now:
+			return False
+
+		message_string = signatureMessageToString(message)
+		try:
+			signer_public_key = pemStringToPublicKey(signer_public_key_string)
+		except:
+			return False
+		try:
+			signature = base64.b64decode(signature_base64)
+		except:
+			return False
+
+		return rsa.verify(message_string, signature, signer_public_key)
+
+	def getAuthorizedKeys(self):
+		return self._config["authorized_keys"]
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import json
 import os
 import shutil
 import glob
@@ -5,14 +6,16 @@ import datetime
 from securep2p227 import keys as sp
 
 # delete the existing key
-for path in glob.glob("test-data/*"):
+for path in glob.glob("test-data/keys/*"):
 	shutil.rmtree(path)
+for path in glob.glob("test-data/permissions/*"):
+	os.remove(path)
 
 # create a host object to connect to specify the server name
 host = sp.Host("securep2p.fivebillionmph.com")
 
 # create a new key with test fake data
-key1 = sp.genKey("test-data", "test1", "Dr. Leonard McCoy", "NCC-1701 USS Enterprise")
+key1 = sp.genKey("test-data/keys", "test1", "Dr. Leonard McCoy", "NCC-1701 USS Enterprise")
 
 # register our key to the host
 key1.register(host)
@@ -24,10 +27,22 @@ session1 = sp.Session(host, key1)
 session1.startSession(8080)
 
 # create a second key and session
-key2 = sp.genKey("test-data", "test2", "Dr. Beverly Crusher", "NCC-1701-D USS Enterprise")
+key2 = sp.genKey("test-data/keys", "test2", "Dr. Beverly Crusher", "NCC-1701-D USS Enterprise")
 key2.register(host)
 session2 = sp.Session(host, key2)
 session2.startSession(8081)
+
+# create a third session
+key3 = sp.genKey("test-data/keys", "test3", "Dr. Alexander Siddig", "Deep Space Nine")
+key3.register(host)
+session3 = sp.Session(host, key3)
+session3.startSession(8082)
+
+# create a fouth session
+key4 = sp.genKey("test-data/keys", "test4", "Dr. Evil", "Starbucks")
+key4.register(host)
+session4 = sp.Session(host, key4)
+session4.startSession(8083)
 
 # sign a key
 now = datetime.datetime.now()
@@ -37,6 +52,12 @@ session1.signKeyAndSubmit(key2._public_key, now, tomorrow)
 # get the active sessions
 active_sessions = session1.getActiveSessions()
 
-# get my signatures (not working yet)
+# get my signatures
 my_signatures = session2.getSignatures()
-print(my_signatures)
+
+permission3 = sp.Permission("test-data/permissions", "ds9-files")
+permission3.addAuthorizedKey(sp.publicKeyToPemString(key1._public_key), "Dr. McCoy", "USS Enterprise")
+# print(permission3.getAuthorizedKeys())
+print("Dr. McCoy", permission3.authorize(sp.publicKeyToPemString(key1._public_key), None, None, None))
+print("Dr. Crusher", permission3.authorize(sp.publicKeyToPemString(key2._public_key), my_signatures["signatures"][0]["signature"], json.loads(my_signatures["signatures"][0]["message"]), my_signatures["signatures"][0]["signer"]["public_key"]))
+print("Dr. Evil", permission3.authorize(sp.publicKeyToPemString(key4._public_key), my_signatures["signatures"][0]["signature"], json.loads(my_signatures["signatures"][0]["message"]), my_signatures["signatures"][0]["signer"]["public_key"]))
